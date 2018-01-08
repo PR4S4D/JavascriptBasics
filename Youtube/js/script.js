837c5ea74feb1720066f70a3fe807d02257927df
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-01-09 00:36:25 +0530

    feat: fancybox youtube player

--- Youtube/js/script.js
+++ Youtube/js/script.js
@@ -178,7 +178,7 @@ function getOutput (item) {
 	'<img src="'+thumb+'">'+
 	'</div>' +
 	'<div class="list-right">' +
-	'<h3>' + title +'</h3>' +
+	'<h3> <a data-fancybox class="fancybox fancybox.iframe iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
 	'<small>By <span class="cTitle">' + channelTitle+ '</span> on '+videoDate + '</small>'+
 	'<p>'+description+'</p>'+
 	'</div>'+
