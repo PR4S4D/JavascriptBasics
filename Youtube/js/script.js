2459a134081f94d9577c31a829abdc722dcc9f98
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-01-08 23:55:45 +0530

    feat: next and prev page fn

--- Youtube/js/script.js
+++ Youtube/js/script.js
@@ -75,6 +75,94 @@ function search() {
 		);	
 }
 
+// Next page fn
+
+function nextPage(){
+
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+
+
+	$('#results').html('');
+
+	$('#buttons').html('');
+
+	// get form input
+	q = $('#query').val();
+
+	// Run get request on API
+
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet,id',
+			q:q,
+			pageToken: token,
+			type:'video',
+			key:'AIzaSyAf4IEgy469xdmJu7VmYT3wOwpfvZrtwd4'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					var output = getOutput(item);
+
+					// display results 
+
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				$('#buttons').append(buttons);
+			}
+		);	
+}
+
+
+// Previous page fn
+
+function prevPage(){
+
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+
+	$('#results').html('');
+
+	$('#buttons').html('');
+
+	// get form input
+	q = $('#query').val();
+
+	// Run get request on API
+
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet,id',
+			q:q,
+			pageToken: token,
+			type:'video',
+			key:'AIzaSyAf4IEgy469xdmJu7VmYT3wOwpfvZrtwd4'},
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				console.log(data);
+
+				$.each(data.items, function(i, item){
+					var output = getOutput(item);
+
+					// display results 
+
+					$('#results').append(output);
+				});
+
+				var buttons = getButtons(prevPageToken, nextPageToken);
+
+				$('#buttons').append(buttons);
+			}
+		);	
+}
 
 function getOutput (item) {
 	var videoId = item.id.videoId;
@@ -105,12 +193,11 @@ function getOutput (item) {
 function getButtons (prevPageToken, nextPageToken) {
 	if(! prevPageToken){
 		var btnoutput = '<div class="button-container">' +
-		'<button id="next-button" class="pagging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' + 'onclick=nextPage();>  Next Page</button></div>';
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' + 'onclick=nextPage();>  Next Page</button></div>';
 	} else{
 		var btnoutput = '<div class="button-container">' +
-		'<button id="next-button" class="pagging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' + 'onclick=prevPage();>  Prev Page</button></div>'; +
-		'<div class="button-container">' +
-		'<button id="next-button" class="pagging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' + 'onclick=nextPage();>  Next Page</button></div>';
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' + 'onclick=prevPage();>  Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' + 'onclick=nextPage();>  Next Page</button></div>';
 	}
 
 	return btnoutput;
